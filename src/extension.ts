6b8f01736d3cecc9fce4e504f8d78e33bc88915b
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2024-05-18 01:58:42 +0300

    Add vscode-yaml dependency

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,54 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {OutputChannel, Uri} from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+const SCHEMA = "myschema";
+
+let log: OutputChannel;
+
+const schemaJSON = JSON.stringify({
+	type: "object",
+	additionalProperties: false,
+	properties: {
+		version: {
+			type: "string",
+			description: "A stringy string string",
+			enum: [
+				"test"
+			]
+		}
+	}
+});
+
+function onRequestSchemaURI(resource: string): string | undefined {
+	log.appendLine('onRequestSchemaURI: ' + resource);
+	if (resource.endsWith('test.yaml')) {
+		return `${SCHEMA}://schema/porter`;
+	}
+	return undefined;
+}
+
+function onRequestSchemaContent(schemaUri: string): string | undefined {
+	log.appendLine('onRequestSchemaContent: ' + schemaUri);
+	const parsedUri = Uri.parse(schemaUri);
+	if (parsedUri.scheme !== SCHEMA) {
+		return undefined;
+	}
+	if (!parsedUri.path || !parsedUri.path.startsWith('/')) {
+		return undefined;
+	}
+
+	return schemaJSON;
+}
+
+export async function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "yaml-schemas" is now active!');
+
+	log = vscode.window.createOutputChannel('YAML Schema Test');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -20,6 +60,18 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+	const yamlExtension = vscode.extensions.getExtension("redhat.vscode-yaml");
+	if(!yamlExtension) {
+		vscode.window.showErrorMessage('vscode-yaml extension not found');
+		return;
+	}
+	const yamlExtensionAPI = await yamlExtension.activate();
+	if(!yamlExtensionAPI) {
+		vscode.window.showErrorMessage('Failed to activate vscode-yaml extension');
+		return;
+	}
+	yamlExtensionAPI.registerContributor(SCHEMA, onRequestSchemaURI, onRequestSchemaContent);
 }
 
 // This method is called when your extension is deactivated
